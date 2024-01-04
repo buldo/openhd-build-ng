41e97cf581fe7377e98df35188259c3ce7853ebb
Roman Buldygin <froooks@gmail.com>
2024-01-05 02:00:59 +0300

    Downloading gcc sources

--- build/Build.cs
+++ build/Build.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using Microsoft.Build.Tasks;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
@@ -10,6 +11,7 @@ using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.IO.HttpTasks;
 
 class Build : NukeBuild
 {
@@ -19,16 +21,72 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.BuildGcc);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    Target Clean => _ => _
-        .Before(Restore)
+    readonly AbsolutePath WorkDir = RootDirectory / "workdir";
+    readonly AbsolutePath ToolchainsDir = RootDirectory / "toolchains";
+    readonly AbsolutePath SysrootsDir = RootDirectory / "sysroots";
+
+    Target CleanWorkdir => _ => _
         .Executes(() =>
         {
+            WorkDir.CreateOrCleanDirectory();
         });
+
+    Target CleanBuildSystem => _ => _
+        .Before(BuildGcc)
+        .Executes(() =>
+        {
+            ToolchainsDir.CreateOrCleanDirectory();
+            ToolchainsDir.CreateOrCleanDirectory();
+        });
+
+    Target BuildGcc => _ => _
+        .Executes(() =>
+        {
+            ToolchainsDir.CreateDirectory();
+            var destinationName = ToolchainsDir / GccDefs.Gcc13ArchiveName;
+            if(!destinationName.FileExists())
+            {
+                Serilog.Log.Information($"{GccDefs.Gcc13ArchiveName} downloading");
+                HttpDownloadFile(
+                    GccDefs.Gcc13SourcesLink, 
+                    destinationName, 
+                    System.IO.FileMode.Create,
+                    c => {
+                        c.Timeout = TimeSpan.FromMinutes(20);
+                        return c;
+                    });
+                Serilog.Log.Information($"{GccDefs.Gcc13ArchiveName} downloaded");
+            }
+            else
+            {
+                Serilog.Log.Information($"{GccDefs.Gcc13ArchiveName} already downloaded");
+            }
+
+            var gccSourcesDir = ToolchainsDir / GccDefs.Gcc13Folder;
+            if(!gccSourcesDir.DirectoryExists())
+            {
+                Serilog.Log.Information($"{GccDefs.Gcc13ArchiveName} extracting");
+                destinationName.UncompressTo(ToolchainsDir / GccDefs.Gcc13Folder);
+                Serilog.Log.Information($"{GccDefs.Gcc13ArchiveName} extracted");
+            }
+            else
+            {
+                Serilog.Log.Information($"{GccDefs.Gcc13ArchiveName} already extracted");
+            }
+        }
+        );
+
+    Target DownloadToolchain => _ => _
+        .Executes(() =>
+        {
+
+        }
+        );
 
     Target Restore => _ => _
         .Executes(() =>
---
+++ build/GccDefs.cs
@@ -0,0 +1,6 @@
+internal static class GccDefs
+{
+    public static string Gcc13SourcesLink => "https://gcc.gnu.org/pub/gcc/releases/gcc-13.2.0/gcc-13.2.0.tar.gz";
+    public static string Gcc13ArchiveName => "gcc-13.2.0.tar.gz";
+    public static string Gcc13Folder => "gcc-13.2.0";
+}
